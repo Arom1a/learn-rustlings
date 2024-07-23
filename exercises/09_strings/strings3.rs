5916bbc8490d7d1d3b7471d97ee668541e7d7649
Aromia <f2940015504@gmail.com>
2024-07-23 13:34:16 -0400

    09 finished

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,19 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut rtn: String = String::from(input);
+    rtn.push_str(" world!");
+    rtn
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let rtn: String = String::from(input);
+    rtn.replace("cars", "balloons")
 }
 
 fn main() {
